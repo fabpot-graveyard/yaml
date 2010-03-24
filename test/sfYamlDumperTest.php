f5b7c92ec4a58c2a76f6bbd35050b6d79a8f4b6f
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-03-24 13:55:40 +0000

    [YAML] added support for compact inline notation with immediate sub mapping (refs #8082)
    git-svn-id: http://svn.symfony-project.com/components/yaml/trunk@28761 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/sfYamlDumperTest.php
+++ test/sfYamlDumperTest.php
@@ -15,7 +15,7 @@ require_once(dirname(__FILE__).'/../lib/sfYamlDumper.php');
 
 sfYaml::setSpecVersion('1.1');
 
-$t = new lime_test(150);
+$t = new lime_test(152);
 
 $parser = new sfYamlParser();
 $dumper = new sfYamlDumper();
